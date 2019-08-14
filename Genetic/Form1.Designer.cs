0b086ec312f67accce1d6c2bdbc653b5498897f8
Raphael Vandon <r.vandon@criteo.com>
2019-08-14 17:09:36 +0200

    real time updated chart

--- Genetic/Form1.Designer.cs
+++ Genetic/Form1.Designer.cs
@@ -29,12 +29,51 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(776, 426);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- Genetic/Form1.cs
+++ Genetic/Form1.cs
@@ -1,20 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Genetic
 {
     public partial class Form1 : Form
     {
+        private readonly Series _series1;
+        private volatile int _current = 0;
+        private readonly DateTime _start;
+
         public Form1()
         {
             InitializeComponent();
+            chart1.Series.Clear();
+            _series1 = new Series
+            {
+                Name = "Series1",
+                ChartType = SeriesChartType.StepLine,
+                BorderWidth = 3,
+            };
+
+            chart1.Series.Add(_series1);
+            _series1.Points.AddXY(0, 0);
+
+            _start = DateTime.UtcNow;
+            Task.Run(() => Walk());
+        }
+
+        private void Walk()
+        {
+            var rand = new Random();
+            var sum = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                Thread.Sleep(300);
+                sum += rand.Next(-10, 11);
+                _current = sum;
+
+                Invoke((MethodInvoker)UpdateChart);
+            }
+        }
+
+        private void UpdateChart()
+        {
+            var now = DateTime.UtcNow;
+            _series1.Points.AddXY((now - _start).TotalSeconds, _current);
+            chart1.Invalidate();
         }
     }
 }
